f0e6c13828218666ee71d5948ccd84a93e06d857
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-03 23:52:03 -0400

    spelling: element
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- include/haproxy/quic_conn-t.h
+++ include/haproxy/quic_conn-t.h
@@ -392,7 +392,7 @@ struct quic_dgram {
 	struct sockaddr_storage daddr;
 	struct quic_conn *qc;
 
-	struct list recv_list; /* elemt to quic_receiver_buf <dgram_list>. */
+	struct list recv_list; /* element to quic_receiver_buf <dgram_list>. */
 	struct mt_list handler_list; /* elem to quic_dghdlr <dgrams>. */
 };
 
