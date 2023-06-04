30eb382eb7811d6cb85893e337f8ca512eb3b1e3
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-03 23:52:02 -0400

    spelling: analyze
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/stream.c
+++ src/stream.c
@@ -1669,7 +1669,7 @@ void stream_update_timings(struct task *t, uint64_t lat, uint64_t cpu)
 
 /* These 2 following macros call an analayzer for the specified channel if the
  * right flag is set. The first one is used for "filterable" analyzers. If a
- * stream has some registered filters, pre and post analyaze callbacks are
+ * stream has some registered filters, pre and post analyze callbacks are
  * called. The second are used for other analyzers (AN_REQ/RES_FLT_* and
  * AN_REQ/RES_HTTP_XFER_BODY) */
 #define FLT_ANALYZE(strm, chn, fun, list, back, flag, ...)			\
